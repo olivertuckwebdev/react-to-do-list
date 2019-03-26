3a7ac609b4696f79920aea7612093ea59d28b734
Oliver Tuck <olivertuckwebdev@gmail.com>
2019-03-26 21:49:42 +0000

    Updates

---
+++ precache-manifest.9da0b1cbf739ffcb72696be83a9da21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a85afaab04600e454541",
+    "url": "/react-to-do-list/static/js/runtime~main.d9a6f0de.js"
+  },
+  {
+    "revision": "53d6d76b88b80c270508",
+    "url": "/react-to-do-list/static/js/main.cbe802db.chunk.js"
+  },
+  {
+    "revision": "d3d4a30ba78ac1d43f7d",
+    "url": "/react-to-do-list/static/js/2.85c4780c.chunk.js"
+  },
+  {
+    "revision": "53d6d76b88b80c270508",
+    "url": "/react-to-do-list/static/css/main.88c6d499.chunk.css"
+  },
+  {
+    "revision": "d3d4a30ba78ac1d43f7d",
+    "url": "/react-to-do-list/static/css/2.37cef203.chunk.css"
+  },
+  {
+    "revision": "d7244a25258de9bacf987e6b7b924fa6",
+    "url": "/react-to-do-list/index.html"
+  }
+];
